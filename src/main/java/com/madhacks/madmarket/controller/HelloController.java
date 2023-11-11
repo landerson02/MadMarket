15f2f14639c0f2c9c0e754346893d55583ae272b
ddjacobson <jacobsondane@icloud.com>
2023-11-11 13:57:34 -0600

    controller

--- src/main/java/com/madhacks/madmarket/controller/HelloController.java
+++ src/main/java/com/madhacks/madmarket/controller/HelloController.java
@@ -1,12 +1,14 @@
 package com.madhacks.madmarket.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping(value = "/test")
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping()
     public String hello() {
         return "Hi";
     }
