d7c7354094f18ccc09d4231538b6537975f9d150
aayushb03 <abbharadwaj@wisc.edu>
2023-11-11 17:25:19 -0600

    Test controller

--- backend/src/main/java/com/madhacks/madmarket/controller/HelloController.java
+++ backend/src/main/java/com/madhacks/madmarket/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.madhacks.madmarket.controller;
 
 import com.madhacks.madmarket.config.CloudSqlConnectionPoolFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,21 +9,28 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/test")
+@CrossOrigin(origins = "http://localhost:4200")
 public class HelloController {
 
 
     @GetMapping()
-    public String hello() throws SQLException {
+    public Map<String, String> hello() throws SQLException {
         DataSource ds = CloudSqlConnectionPoolFactory.createConnectionPool();
         ResultSet rs = ds.getConnection().prepareStatement("select * from users").executeQuery();
 
         while (rs.next()) {
             System.out.println("username: " + rs.getString("username"));
         }
-        return "Hi";
+
+        Map<String, String> jsonResponse = new HashMap<>();
+        jsonResponse.put("message", "Hello from the server!");
+
+        return jsonResponse;
     }
 
 }
