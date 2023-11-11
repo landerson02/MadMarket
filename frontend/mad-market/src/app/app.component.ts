eac004f0c275c684c2a9b7e74320994afcbeb21c
aayushb03 <abbharadwaj@wisc.edu>
2023-11-11 17:26:36 -0600

    Various changes

--- frontend/mad-market/src/app/app.component.ts
+++ frontend/mad-market/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from "./services/api.service";
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor() {
-
+  constructor(private apiService: ApiService) {
+    this.getTest();
   }
-  title = 'mad-market';
+
+  getTest() {
+    this.apiService.getTest().subscribe(data => {
+      console.log(data);
+    });
+  }
 }
--- frontend/mad-market/src/app/app.module.ts
+++ frontend/mad-market/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ApiService } from "./services/api.service";
 
 @NgModule({
   declarations: [
@@ -11,10 +13,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     HeaderComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     NgbModule
   ],
-  providers: [],
+  providers: [
+    ApiService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
