989409d98bce47f05c9ced7d6bccca537a2fff4c
aayushb03 <abbharadwaj@wisc.edu>
2023-11-11 19:29:28 -0600

    Controller update

--- backend/src/main/java/com/madhacks/madmarket/controller/ListingController.java
+++ backend/src/main/java/com/madhacks/madmarket/controller/ListingController.java
@@ -2,20 +2,17 @@ package com.madhacks.madmarket.controller;
 
 import com.madhacks.madmarket.service.ListingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-
+@CrossOrigin(origins = "http://localhost:4200")
 public class ListingController {
 
     @Autowired
     ListingService listingService;
 
-    @PostMapping("/getListingsByCategory")
-    public String getListingsByCategory(@RequestBody final long categoryId) {
+    @GetMapping("/getListingsByCategory")
+    public String getListingsByCategory(long categoryId) {
         return listingService.getListingsByCategory(categoryId).toString();
     }
 
