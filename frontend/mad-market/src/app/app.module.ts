6a4772578e36b44ea5b48b05c8b2638584b7456a
aayushb03 <abbharadwaj@wisc.edu>
2023-11-11 20:14:50 -0600

    Various changes

--- frontend/mad-market/src/app/app.module.ts
+++ frontend/mad-market/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ApiService } from "./services/api.service";
 import { SigninComponent } from './components/signin/signin.component';
 import { SavedComponent } from './components/saved/saved.component';
+import { CategorylistingsComponent } from './components/categorylistings/categorylistings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SigninComponent,
-    SavedComponent
+    SavedComponent,
+    CategorylistingsComponent
   ],
   imports: [
     HttpClientModule,
