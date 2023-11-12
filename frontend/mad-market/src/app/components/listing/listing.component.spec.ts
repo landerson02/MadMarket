9380489bd41fe7893b0fa5a28062bc7ae8b000f0
Lucas Anderson <lucas.a@comcast.net>
2023-11-11 20:16:27 -0600

    Update signin component and add listing component

---
+++ frontend/mad-market/src/app/components/listing/listing.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListingComponent } from './listing.component';
+
+describe('ListingComponent', () => {
+  let component: ListingComponent;
+  let fixture: ComponentFixture<ListingComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ListingComponent]
+    });
+    fixture = TestBed.createComponent(ListingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
