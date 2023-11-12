d7dd9b89bffe228d853d6d0e62f49063fe04b2a5
ddjacobson <jacobsondane@icloud.com>
2023-11-11 21:38:49 -0600

    user stuff

--- backend/src/main/java/com/madhacks/madmarket/controller/UserController.java
+++ backend/src/main/java/com/madhacks/madmarket/controller/UserController.java
@@ -1,10 +1,9 @@
 package com.madhacks.madmarket.controller;
 
-
 import com.madhacks.madmarket.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +13,7 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @PostMapping("/addUser")
+    @GetMapping("/addUser")
     public void signUpUser(String name, String email, String phone) {
         userService.addUser(name, email, phone);
     }
