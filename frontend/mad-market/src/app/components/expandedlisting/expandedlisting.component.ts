e0e98fed45c8b4d1416e1efba0a1722800d6deb5
Lucas Anderson <lucas.a@comcast.net>
2023-11-12 00:43:06 -0600

    Add expandedlisting component and listing description field

---
+++ frontend/mad-market/src/app/components/expandedlisting/expandedlisting.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expandedlisting',
+  templateUrl: './expandedlisting.component.html',
+  styleUrls: ['./expandedlisting.component.css']
+})
+export class ExpandedlistingComponent {
+
+}
