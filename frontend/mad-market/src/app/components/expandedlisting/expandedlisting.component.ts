958f444793174e42f2126c638aa68c5bf74d7b2f
Lucas Anderson <lucas.a@comcast.net>
2023-11-12 06:50:45 -0600

    Fix categories

--- frontend/mad-market/src/app/components/expandedlisting/expandedlisting.component.ts
+++ frontend/mad-market/src/app/components/expandedlisting/expandedlisting.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Listing} from "../../objects/listing";
+import {Category} from "../../objects/category";
 
 @Component({
   selector: 'app-expandedlisting',
@@ -8,9 +9,15 @@ import {Listing} from "../../objects/listing";
 })
 export class ExpandedlistingComponent {
   @Input() listing?: Listing;
+  @Input() userMode?: boolean;
+  @Input() categories? : Category[];
   @Output() public goBack = new EventEmitter<void>();
 
   onGoBack() {
     this.goBack.emit();
+  }
+
+  getCategoryName(categoryId: number) {
+    return this.categories?.find(category => category.id == categoryId)?.name;
   }
 }
--- frontend/mad-market/src/app/components/profile/profile.component.ts
+++ frontend/mad-market/src/app/components/profile/profile.component.ts
@@ -1,11 +1,30 @@
-import {Component, Input} from '@angular/core';
-import {Model} from "../../objects/model";
+import {Component, Input, OnChanges} from '@angular/core';
+import {User} from "../../objects/user";
+import {Listing} from "../../objects/listing";
+import {Category} from "../../objects/category";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
-  @Input() model?: Model;
+export class ProfileComponent implements OnChanges {
+  @Input() user?: User;
+  @Input() categories?: Category[];
+  @Input() expandedView: boolean = false;
+  public exView: boolean = false;
+  public selectedListing?: Listing;
+
+  ngOnChanges(): void {
+    this.exView = this.expandedView;
+  }
+
+  onExpandedView(listing: Listing) {
+    this.exView = true;
+    this.selectedListing = listing;
+  }
+
+  onGoBack() {
+    this.exView = false;
+  }
 }
